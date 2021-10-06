2446b5b9432f91d170b043c08beda43242241416
Heraclitus <03m3333@gmail.com>
2021-10-06 14:15:25 -0700

    Adding javadocs

---
+++ spiralingmatrixwaker/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"me.alfreds.matrixwalker","l":"MatrixWalkerApplication.Direction"},{"p":"me.alfreds.matrixwalker","l":"MatrixWalkerApplication"}]
